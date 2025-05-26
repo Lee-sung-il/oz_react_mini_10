91708a949ec62457c3d6bdf7c0424ca22dde1374
Lee-sung-il <wj132612@naver.com>
2025-05-26 10:58:00 +0900

    데이터베이스 연동

--- movie_list_react_app_server/server/index.js
+++ movie_list_react_app_server/server/index.js
@@ -9,8 +9,9 @@ require('dotenv').config(); // 이 줄이 있어야 .env 파일이 적용됩니�
 
 const app = express();
 const PORT = 8080;
+const DB_URL = process.env.DB_URL || 'mongodb://localhost:27017/movieUsers';
 
-mongoose.connect('mongodb://localhost:27017/movieUsers')
+mongoose.connect(DB_URL)
     .then(() => console.log('✅ MongoDB 연결 성공'))
     .catch((err) => console.error('❌ MongoDB 연결 실패:', err));
 
